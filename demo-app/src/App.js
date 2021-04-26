e708312d8ee01852fc78af984c747866310bf3d6
Nila <Nila@LAPTOP-M0QERO78>
2021-04-26 17:48:40 -0400

    main page design

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
+import Home from "./components/Home.js"
+import AccountProfile from "./components/AccountProfile.js"
 import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles, useTheme, withStyles } from '@material-ui/core/styles';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
-import NearMeIcon from '@material-ui/icons/NearMe';
+import FindInPageRoundedIcon from '@material-ui/icons/FindInPageRounded';
 import EventNoteIcon from '@material-ui/icons/EventNote';
-import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
-import {
+import ExitToAppRoundedIcon from '@material-ui/icons/ExitToAppRounded';
+import PostAddRoundedIcon from '@material-ui/icons/PostAddRounded';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import SignUp from "./components/SignUp"
+import DehazeSharpIcon from '@material-ui/icons/DehazeSharp';import {
   Button,
   List,
   ListItemText,
@@ -27,6 +31,12 @@ import {
   Grid,
 } from "@material-ui/core";
 const drawerWidth = 240;
+
+const TypographyStyle = withStyles({
+  root: {
+    color: "#375C23",
+  }
+})(Typography);
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -85,11 +95,11 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function App() {
+export default function App() {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
-
+  
   const handleDrawerOpen = () => {
     setOpen(true);
   };
@@ -98,104 +108,145 @@ function App() {
     setOpen(false);
   };
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <Typography variant="h6" noWrap className={classes.title}>
-          </Typography>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="end"
-            onClick={handleDrawerOpen}
-            className={clsx(open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
+  const handleAccount = (e) => {
+    e.preventDefault()
+  }
+return(
+  <div className={classes.root}>
+    <CssBaseline />
+    <AppBar
+      position="fixed"
+      style={{background:"#C2F0AA"}}
+      className={clsx(classes.appBar, {
+        [classes.appBarShift]: open,
+      })}
+    >
+    <Toolbar>
+        <Typography variant="h6" noWrap className={classes.title} >
+        </Typography>
+        <IconButton
+          aria-label="open drawer"
+          edge="end"
+          onClick={handleDrawerOpen}
+          className={clsx(open && classes.hide)}
+        >
+          <DehazeSharpIcon />
+        </IconButton>
+      </Toolbar>
       </AppBar>
       <main
-        className={clsx(classes.content, {
-          [classes.contentShift]: open,
-        })}
-      >
-        <div className={classes.drawerHeader} />
-        <Typography paragraph>
-        Add logo etc
-        </Typography>
-      </main>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="right"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </div>
-        <Divider />
-          {['My Account', 'My Notes'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <PersonOutlineIcon />: <EventNoteIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-          {['Saved Doctors', 'Find Doctors'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <PeopleOutlineIcon />: <NearMeIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        <Divider />
-        <List>
-        {['About Us'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon><PeopleOutlineIcon /></ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+      className={clsx(classes.content, {
+        [classes.contentShift]: open,
+      })}
+    >
+      <div className={classes.drawerHeader} />
+      <Typography paragraph>
+      Possible name : Mentality.com
+      </Typography>
+    </main>
+    <Drawer
+      className={classes.drawer}
+      variant="persistent"
+      anchor="right"
+      open={open}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <div className={classes.drawerHeader}>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === 'rtl' ? <ChevronLeftIcon style={{color:'#375C23'}}/> : <ChevronRightIcon style={{color:'#375C23'}}/>}
+        </IconButton>
+      </div>
+      <Divider />
+      <Router>
+      <TypographyStyle>
+      {['Home'].map((text, index) => (
+          <ListItem 
+          button key={text}
+          component={Link} to='/'
+          >
+            <ListItemIcon><PersonOutlineIcon style={{color:'#375C23'}}/></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+        {['My Account'].map((text, index) => (
+          <ListItem 
+          button key={text}
+          component={Link} to='/my-account'
+          href="/my-account"
+          >
+            <ListItemIcon><PersonOutlineIcon style={{color:'#375C23'}}/></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+        {['My Notes'].map((text, index) => (
+          <ListItem 
+          button key={text}
+          component={Link} to='/my-notes'
+          >
+            <ListItemIcon><EventNoteIcon style={{color:'#375C23'}}/></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+       {['Saved Doctors'].map((text, index) => (
+          <ListItem 
+          button key={text}
+          component={Link} to='/saved-doctors'
+          >
+            <ListItemIcon><PeopleOutlineIcon style={{color:'#375C23'}}/></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+       {['Find Doctors'].map((text, index) => (
+          <ListItem 
+          button key={text}
+          component={Link} to='/find-doctor'
+          >
+            <ListItemIcon><FindInPageRoundedIcon style={{color:'#375C23'}}/></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      <Divider />
+      <List>
+      {['Sign In'].map((text, index) => (
+          <ListItem 
+          button key={text}
+          component={Link} to='/sign-in'
+          >
+            <ListItemIcon><ExitToAppRoundedIcon style={{color:'#375C23'}}/></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      {['Register'].map((text, index) => (
+          <ListItem 
+          button key={text}
+          component={Link} to='/register'
+          >
+            <ListItemIcon><PostAddRoundedIcon style={{color:'#375C23'}}/></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      </TypographyStyle>
+
+      </Router>
       </Drawer>
+
+      <Router>
+        <Switch>
+          <Route path to="/my-account" >
+            <AccountProfile />
+          </Route>
+          <Route path to="/sign-up">
+            <SignUp />
+          </Route>
+        </Switch>
+      </Router>
+
+
     </div>
-  );
+)
 }
 
 
-// import logo from './logo.svg';
-// import './App.css';
-// import React from 'react';
-// function App() {
-//   return (
-//     <div className="App">
-//     <p>hi</p>
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Welcome <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-export default App;
